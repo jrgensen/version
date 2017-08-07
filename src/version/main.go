03f257502ba719313ea4f5fd55fdfdba68e919bc
Klaus Jrgensen <klaus@blackwoodseven.com>
2017-08-07 14:07:29 +0200

    output is now project aware

--- src/version/main.go
+++ src/version/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
-	//	"fmt"
+	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"log"
@@ -12,14 +12,23 @@ import (
 )
 
 type Container struct {
-	Names   []string
-	Image   string
-	Created int64
-	Labels  map[string]string
-	State   string
-	Status  string
+	ServiceName    string
+	ContainerNames []string
+	Image          types.ImageSummary
+	Created        int64
+	Labels         map[string]string
+	State          string
+	Status         string
+	SizeRootFs     int64
 }
+
+type Project struct {
+	ProjectName string
+	Containers  []Container
+}
+
 type Response struct {
+	Projects   map[string]Project
 	Containers []Container
 }
 
@@ -30,32 +39,49 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	images := make(map[string]types.ImageSummary)
+	imageSummaries, err := cli.ImageList(context.Background(), types.ImageListOptions{})
 	if err != nil {
 		panic(err)
 	}
-
-	cs := []Container{}
-	for _, container := range containers {
-		cs = append(cs, Container{
-			Names:   container.Names,
-			Image:   container.Image,
-			Created: container.Created,
-			Labels:  container.Labels,
-			State:   container.State,
-			Status:  container.Status,
-		})
-		//fmt.Printf("%s %s\n", container.ID[:10], "")
+	for _, imageSummary := range imageSummaries {
+		images[imageSummary.ID] = imageSummary
 	}
-	jsonstr, err := json.Marshal(cs)
+
+	resp := Response{
+		Projects: make(map[string]Project),
+	}
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{Size: true})
+	for _, container := range containers {
+		c := Container{
+			ContainerNames: container.Names,
+			Image:          images[container.ImageID],
+			Created:        container.Created,
+			Labels:         container.Labels,
+			State:          container.State,
+			Status:         container.Status,
+			SizeRootFs:     container.SizeRootFs,
+		}
+		if projectName, ok := c.Labels["com.docker.compose.project"]; ok {
+			c.ServiceName = c.Labels["com.docker.compose.service"]
+			project := resp.Projects[projectName]
+			project.ProjectName = projectName
+			project.Containers = append(project.Containers, c)
+
+			resp.Projects[projectName] = project
+		} else {
+			resp.Containers = append(resp.Containers, c)
+		}
+		js, _ := json.Marshal(container)
+		fmt.Printf("%s %s\n", container.ID[:10], js)
+	}
+	jsonstr, err := json.Marshal(resp)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonstr)
-
-	//fmt.Fprintf(w, "%s", jsonstr)
 }
 func main() {
 	log.SetFlags(log.Lshortfile)
